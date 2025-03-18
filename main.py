2a3c3f9f13c58a1f35938804938c3f825fb16f11
wochieng9 <ocu9@cdc.gov>
2025-03-18 14:22:55 -0400

    Update main.py
    Changed word limit to 5000

--- main.py
+++ main.py
@@ -153,13 +153,13 @@ def main():
                         st.session_state.regenerate_counter = 0
                     words = text.split()
                     numwords = len(words)
-                    if numwords > 3000:
-                        st.error(f"Text is too long ({numwords} words). Maximum allowed is 3000 words. Please reduce the text length and try again. Can delete references.")
+                    if numwords > 5000:
+                        st.error(f"Text is too long ({numwords} words). Maximum allowed is 5000 words. Please reduce the text length and try again. Can delete references.")
                         st.stop()
                     max_words = st.sidebar.slider(
                         "Maximum desired word length",
                         min_value = 100,
-                        max_value = 3000,
+                        max_value = 5000,
                         value = numwords,
                         step = 50,
                         help = "Limit the length of text to <3000 for analysis"
